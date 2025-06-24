98c98789fdbe4d51769632392ce8508dc1796c8c
Pedro Victor <103965597+pedrovi34@users.noreply.github.com>
2025-06-23 21:49:41 -0300

    Update stack.c

--- stack.c
+++ stack.c
@@ -3,8 +3,8 @@
 #include "stack.h"
 
 Stack* new_stack(int size) {
-    Stack* s = (Stack*)malloc(sizeof(Stack));
-    s->data = (int*)malloc(sizeof(int) * size);
+    Stack* s = malloc(sizeof(Stack));
+    s->data = malloc(sizeof(int) * size);
     s->top = -1;
     s->size = size;
     return s;
@@ -14,7 +14,7 @@ void stack_push(Stack* s, int value) {
     if (s->top < s->size - 1) {
         s->data[++s->top] = value;
     } else {
-        printf("Pilha cheia!\n");
+        printf("Erro: Pilha cheia!\n");
     }
 }
 
@@ -22,13 +22,12 @@ int stack_pop(Stack* s) {
     if (s->top >= 0) {
         return s->data[s->top--];
     } else {
-        printf("Pilha vazia!\n");
+        printf("Erro: Pilha vazia!\n");
         return -1;
     }
 }
 
 void stack_print(Stack* s) {
-    printf("Conteúdo da pilha: ");
     for (int i = 0; i <= s->top; i++) {
         printf("%d ", s->data[i]);
     }
